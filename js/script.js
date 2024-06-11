f9de6f6e8751155e9576809b2190e1d661557fa9
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-06-12 02:59:11 +0400

    feature/section4
    adding section4 (swiper)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
-AOS.init();
-
 const btn = document.querySelector('.menu_btn');
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open');
+});
+
+AOS.init();
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.right_arrow',
+    prevEl: '.left_arrow',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    850: {
+      slidesPerView: 2,
+    },
+
+    1100: {
+      slidesPerView: 3,
+    },
+  },
 });
