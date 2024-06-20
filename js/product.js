6e318471248a890ec6776accfae03c16327d3edc
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-06-21 00:02:40 +0400

    feature/product page
    adding product page and style

--- js/product.js
+++ js/product.js
@@ -18,7 +18,8 @@ async function adIindividualPage() {
     console.log(product);
 
     if (product) {
-      const img = document.getElementById('img');
+      const img = document.querySelector('.peoduct_img');
+      img.src = `essets/webp/main/products_page/${product.img}`;
 
       const newP = document.getElementById('newProduct');
       newP.textContent = product.new;
@@ -30,7 +31,25 @@ async function adIindividualPage() {
       info.textContent = product.description;
 
       const price = document.getElementById('productPrice');
-      price.textContent = product.price;
+      price.textContent = `$ ${product.price}`;
+
+      const imgArray = product.additionalImages;
+
+      const imgContainer = document.getElementById('imgContainer');
+
+      imgArray.forEach((img, index) => {
+        let imgclass = `img_${index + 1}`;
+        let imgBoxclass = `imgBox_${index + 1}`;
+        let imgBox = document.createElement('div');
+        imgBox.className = imgBoxclass;
+
+        let imgs = document.createElement('img');
+        imgs.src = `./essets/webp/main/products_page/product_page/${img}`;
+        imgs.className = imgclass;
+
+        imgBox.appendChild(imgs);
+        imgContainer.appendChild(imgBox);
+      });
     }
   } catch (error) {
     console.error('Error');
