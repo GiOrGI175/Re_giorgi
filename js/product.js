38d854c05a3a9a24d9e13373c4cfcee7a55d47ce
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-06-28 03:09:07 +0400

    feature/popup
    adding logic

--- js/product.js
+++ js/product.js
@@ -7,76 +7,76 @@ const productId = parseInt(params.get('id'));
 async function adIindividualPage() {
   let url = 'products.json';
 
-  try {
-    let response = await fetch(url);
-    let data = await response.json();
+  // try {
+  let response = await fetch(url);
+  let data = await response.json();
 
-    products = data;
+  products = data;
 
-    const product = products.find((product) => product.id === productId);
+  const product = products.find((product) => product.id === productId);
 
-    console.log(product);
+  console.log(product);
 
-    if (product) {
-      const img = document.querySelector('.peoduct_img');
-      img.src = `essets/webp/main/products_page/${product.img}`;
+  if (product) {
+    const img = document.querySelector('.peoduct_img');
+    img.src = `essets/webp/main/products_page/${product.img}`;
 
-      const newP = document.getElementById('newProduct');
-      newP.textContent = product.new;
+    const newP = document.getElementById('newProduct');
+    newP.textContent = product.new;
 
-      const name = document.getElementById('productName');
-      name.textContent = product.name;
+    const name = document.getElementById('productName');
+    name.textContent = product.name;
 
-      const info = document.getElementById('productinfo');
-      info.textContent = product.description;
+    const info = document.getElementById('productinfo');
+    info.textContent = product.description;
 
-      const price = document.getElementById('productPrice');
-      price.textContent = `$ ${product.price}`;
+    const price = document.getElementById('productPrice');
+    price.textContent = `$ ${product.price}`;
 
-      const quantityInput = document.querySelector('.input_elemnt');
-      quantityInput.id = `quantity-${product.id}`;
+    const quantityInput = document.querySelector('.input_elemnt');
+    quantityInput.id = `quantity-${product.id}`;
 
-      const decrementButton = document.getElementById('decr');
+    const decrementButton = document.getElementById('decr');
 
-      const incrementButton = document.getElementById('incr');
+    const incrementButton = document.getElementById('incr');
 
-      incrementButton.addEventListener('click', () =>
-        incrementQuantity(product.id)
+    incrementButton.addEventListener('click', () =>
+      incrementQuantity(product.id)
+    );
+
+    decrementButton.addEventListener('click', () =>
+      decrementQuantity(product.id)
+    );
+
+    const addButton = document.querySelector('.add_btn');
+    addButton.addEventListener('click', () => {
+      const quantity = parseInt(
+        document.querySelector(`#quantity-${product.id}`).value
       );
+      addToCart({ ...product, quantity });
+    });
 
-      decrementButton.addEventListener('click', () =>
-        decrementQuantity(product.id)
-      );
+    const imgArray = product.additionalImages;
 
-      const addButton = docuemn.querySelector('.add_btn');
-      addButton.addEventListener('click', () => {
-        const quantity = parseInt(
-          document.querySelector(`#quantity-${product.id}`).value
-        );
-        addToCart({ ...product, quantity });
-      });
+    const imgContainer = document.getElementById('imgContainer');
 
-      const imgArray = product.additionalImages;
+    imgArray.forEach((img, index) => {
+      let imgclass = `img_${index + 1}`;
+      let imgBoxclass = `imgBox_${index + 1}`;
+      let imgBox = document.createElement('div');
+      imgBox.className = imgBoxclass;
 
-      const imgContainer = document.getElementById('imgContainer');
+      let imgs = document.createElement('img');
+      imgs.src = `./essets/webp/main/products_page/product_page/${img}`;
+      imgs.className = imgclass;
 
-      imgArray.forEach((img, index) => {
-        let imgclass = `img_${index + 1}`;
-        let imgBoxclass = `imgBox_${index + 1}`;
-        let imgBox = document.createElement('div');
-        imgBox.className = imgBoxclass;
-
-        let imgs = document.createElement('img');
-        imgs.src = `./essets/webp/main/products_page/product_page/${img}`;
-        imgs.className = imgclass;
-
-        imgBox.appendChild(imgs);
-        imgContainer.appendChild(imgBox);
-      });
-    }
-  } catch (error) {
-    console.error('Error');
+      imgBox.appendChild(imgs);
+      imgContainer.appendChild(imgBox);
+    });
   }
+  // } catch (error) {
+  //   console.error('Error');
+  // }
 }
 adIindividualPage();
 
@@ -101,10 +101,10 @@ function addToCart(product) {
   if (productindex >= 0) {
     cart[productindex].quantity += product.quantity;
   } else {
-    cart.push;
+    cart.push(product);
   }
 
-  localStorage.setItem('cart ', JSON.stringify(cart));
+  localStorage.setItem('cart', JSON.stringify(cart));
 
   updateCartPopup();
 }
@@ -117,4 +117,64 @@ function calculateTotalQuantity(cart) {
   return cart.reduce((sum, p) => sum + Number(p.quantity), 0);
 }
 
-function updateCartPopup() {}
+function updateCartPopup() {
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+  console.log(`rame${cart}`);
+
+  let total = calculateTotalPrice(cart);
+
+  let items = calculateTotalQuantity(cart);
+
+  const popup = document.querySelector('.popup');
+
+  console.log(popup);
+
+  popup.textContent = '';
+
+  cart.forEach((product, index) => {
+    console.log(product);
+    const productBox = document.createElement('div');
+
+    const imgBox = document.createElement('div');
+
+    const nameAndPriceBox = document.createElement('div');
+    nameAndPriceBox.className = 'name_price_container';
+
+    const img = document.createElement('img');
+    img.src = `essets/webp/main/products_page/${product.img}`;
+    imgBox.appendChild(img);
+    productBox.appendChild(imgBox);
+
+    const name = document.createElement('h2');
+    name.textContent = product.name;
+    nameAndPriceBox.appendChild(name);
+
+    const price = document.createElement('span');
+    price.textContent = product.price;
+    nameAndPriceBox.appendChild(price);
+
+    productBox.appendChild(nameAndPriceBox);
+
+    const quantity = document.createElement('span');
+    quantity.textContent = product.quantity;
+    quantity.className = 'price';
+    productBox.appendChild(quantity);
+
+    const button = document.createElement('button');
+    button.textContent = 'Remove';
+    button.addEventListener('click', function () {
+      cart.splice(index, 1);
+
+      localStorage.setItem('cart', JSON.stringify(cart));
+
+      updateCartPopup();
+    });
+
+    console.log(productBox);
+    productBox.appendChild(button);
+
+    console.log(productBox);
+    popup.appendChild(productBox);
+  });
+}
